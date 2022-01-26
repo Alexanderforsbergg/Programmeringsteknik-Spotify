9b8fe72cca1dfe4455162b66217d8e8728a1ad41
Alexanderforsbergg <alexanderforsbergg@gmail.com>
2022-01-26 20:45:12 +0100

    first commit

---
+++ SpotifyCore/SpotifyCore/Album.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpotifyCore
+{
+    class Album: IInformation
+    {
+        public string AlbumNamn { get; set; }
+
+        public int ReleaseDatum { get; set; }
+
+
+        public string GetInformation()
+        {
+            return $"Namnet på albumet är {AlbumNamn} och släpptes {ReleaseDatum}";
+        }
+    }
+}
